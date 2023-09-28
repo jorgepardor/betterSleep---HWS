a0b1843c11802b28653ab23ab51d04b6ef47dcda
Jorge Pardo <hi@jorgepardo.dev>
2023-09-28 10:45:27 +0200

    Initial Commit

---
+++ betterSleep/betterSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterSleepApp.swift
+//  betterSleep
+//
+//  Created by Jorge Pardo on 28/9/23.
+//
+
+import SwiftUI
+
+@main
+struct betterSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
